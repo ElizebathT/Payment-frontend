dee7e13c887f32c30d47027780a078521b909ced
Elizebath <elizebaththomasv@gmail.com>
2025-02-25 12:55:09 +0530

    upi

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,16 +3,10 @@ import React from 'react'
 import { paymentVerifyAPI } from '../services/payments'
 import { useSearchParams } from 'react-router-dom'
 
-const Success = () => {  
-  // const {data,isError,isLoading, isSuccess}=useQuery({
-  //   queryKey:["verify-payment"],
-  //   queryFn:paymentVerifyAPI
-  // })
-  
+const Success = () => { 
   return (
     <div>
       <p>Success</p>
-      {/* {data} */}
     </div>
   )
 }
